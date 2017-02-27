90392ad08f12a5394016d0abdf8bc25cabc097aa
Steve Walsh <stephen.walsh1337@gmail.com>
2017-02-27 15:20:41 +0000

    added func for mobile api calls

--- api/mobile/mobile.controller.js
+++ api/mobile/mobile.controller.js
@@ -15,19 +15,19 @@
     } ;
 
     
-    exports.create = function(req, res) {
-      console.log("On home page")
-      console.log(req.body.encoded_string)
-      console.log(req.body.name)
-      console.log(req.body.image)
-      res.send('Image uploaded')
-      var date = Date.now();
-      var name = date + "_"+ req.body.image
-      var fs = require("fs");
+//     exports.create = function(req, res) {
+//       console.log("On home page")
+//       console.log(req.body.encoded_string)
+//       console.log(req.body.name)
+//       console.log(req.body.image)
+//       res.send('Image uploaded')
+//       var date = Date.now();
+//       var name = date + "_"+ req.body.image
+//       var fs = require("fs");
 
-      fs.writeFile("./pictures/" +name, new Buffer(req.body.encoded_string, "base64"), function(err) {});
+//       fs.writeFile("./pictures/" +name, new Buffer(req.body.encoded_string, "base64"), function(err) {});
 
-};
+// };
 
 
    
--- routes.js
+++ routes.js
@@ -1,7 +1,7 @@
 module.exports = function(app) {
 
   app.use('/api/users', require('./api/user/index'));
-  app.use('/api/mobile', require('./api/mobile/index'));
+  app.use('/api/mobiles', require('./api/mobile/index'));
   app.use('/api/events', require('./api/event/index'));
   app.use('/api/pictures', require('./api/picture/index'));
   

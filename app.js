0b4f3af6ae13e523fb5f2088303ef822f02d5bc2
--global <its.my.escape@gmail.com>
2017-04-08 14:11:10 +0100

    added basic socket.io

--- app.js
+++ app.js
@@ -137,7 +137,73 @@ app.post('/authenticate', function (req, res) {
   })
 });
 require('./routes')(app)
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 
-app.listen(port, function() {
-  console.log('Express server listening on port : '+ port);
+
+// io.on('connection', function(){ 
+//   /* … */ 
+// });
+// server.listen(3000);
+
+
+io.on('connection', function (socket) {
+  var addedUser = false;
+
+  // when the client emits 'new message', this listens and executes
+  socket.on('new message', function (data) {
+    // we tell the client to execute 'new message'
+    socket.broadcast.emit('new message', {
+      username: socket.username,
+      message: data
+    });
+  });
+
+  // when the client emits 'add user', this listens and executes
+  socket.on('add user', function (username) {
+    if (addedUser) return;
+
+    // we store the username in the socket session for this client
+    socket.username = username;
+    socket.room = room
+    addedUser = true;
+    socket.emit('login', {
+      numUsers: numUsers
+    });
+    // echo globally (all clients) that a person has connected
+    // socket.broadcast.emit('user joined', {
+    //   username: socket.username,
+    //   numUsers: numUsers
+    // });
+  });
+
+  // when the client emits 'typing', we broadcast it to others
+  // socket.on('typing', function () {
+  //   socket.broadcast.emit('typing', {
+  //     username: socket.username
+  //   });
+  // });
+
+  // // when the client emits 'stop typing', we broadcast it to others
+  // socket.on('stop typing', function () {
+  //   socket.broadcast.emit('stop typing', {
+  //     username: socket.username
+  //   });
+  // });
+
+  // when the user disconnects.. perform this
+  // socket.on('disconnect', function () {
+  //   if (addedUser) {
+  //     // echo globally that this client has left
+  //     socket.broadcast.emit('user left', {
+  //       username: socket.username,
+  //       numUsers: numUsers
+  //     });
+  //   }
+  // });
 });
+
+
+  server.listen(port, function() {
+    console.log('Express server listening on port : '+ port);
+  });
